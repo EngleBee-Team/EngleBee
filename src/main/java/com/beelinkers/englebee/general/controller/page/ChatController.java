7ffd5eb8e9f2155f7cebd07cecbd280ce867f9ed
haram <rlagkfka9611@naver.com>
2024-08-23 15:09:50 +0900

    chore: 회원 role에 따른 뷰 변경을 위해 임시 member 정보 작성

--- src/main/java/com/beelinkers/englebee/general/controller/page/ChatController.java
+++ src/main/java/com/beelinkers/englebee/general/controller/page/ChatController.java
@@ -1,14 +1,22 @@
 package com.beelinkers.englebee.general.controller.page;
 
+import com.beelinkers.englebee.auth.domain.entity.Member;
+import com.beelinkers.englebee.auth.domain.entity.Role;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class ChatController {
   @GetMapping("/chat")
-  public String goToSocketPage() {
-    return "general/socket";
+  public String goToSocketPage(Model model) {
+    Member tempMember = Member.builder()
+        .nickname("teacher1")
+        .role(Role.TEACHER)
+        .build();
+    model.addAttribute("member", tempMember);
+    return "general/webrtc";
   }
 }
