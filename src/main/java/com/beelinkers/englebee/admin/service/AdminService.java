39b31053614f10e82ec5faeb02fc117c0e00321f
haram <rlagkfka9611@naver.com>
2024-08-27 10:20:31 +0900

    feat: subject Level 별 멤버 수를 담기 위한 service 로직 생성

--- src/main/java/com/beelinkers/englebee/admin/service/AdminService.java
+++ src/main/java/com/beelinkers/englebee/admin/service/AdminService.java
@@ -1,5 +1,13 @@
 package com.beelinkers.englebee.admin.service;
 
+import com.beelinkers.englebee.admin.dto.response.AdminMemberSubjectLevelCountDTO;
+import java.util.List;
+
 public interface AdminService {
 
+  List<AdminMemberSubjectLevelCountDTO> getChartUserLevelGrammer();
+
+  List<AdminMemberSubjectLevelCountDTO> getChartUserLevelSentence();
+
+  List<AdminMemberSubjectLevelCountDTO> getChartUserLevelWord();
 }
--- src/main/java/com/beelinkers/englebee/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/beelinkers/englebee/admin/service/impl/AdminServiceImpl.java
@@ -1,9 +1,34 @@
 package com.beelinkers.englebee.admin.service.impl;
 
+import com.beelinkers.englebee.admin.dto.response.AdminMemberSubjectLevelCountDTO;
 import com.beelinkers.englebee.admin.service.AdminService;
+import com.beelinkers.englebee.general.domain.repository.MemberSubjectLevelRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
+
+  private final MemberSubjectLevelRepository memberSubjectLevelRepository;
+
+  @Override
+  public List<AdminMemberSubjectLevelCountDTO> getChartUserLevelGrammer() {
+    return memberSubjectLevelRepository.findLevelCountsGrammer();
+  }
+
+  @Override
+  public List<AdminMemberSubjectLevelCountDTO> getChartUserLevelSentence() {
+    return memberSubjectLevelRepository.findLevelCountsSentence();
+  }
+
+  @Override
+  public List<AdminMemberSubjectLevelCountDTO> getChartUserLevelWord() {
+    return memberSubjectLevelRepository.findLevelCountsWord();
+  }
+
 
 }
