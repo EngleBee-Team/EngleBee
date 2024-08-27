0a5b9289ffe1ffdcf1abaf1c70dda387cc297c7f
haram <rlagkfka9611@naver.com>
2024-08-27 10:20:00 +0900

    feat: subject Level 별 멤버 수를 담기 위한 controller 로직 생성

--- src/main/java/com/beelinkers/englebee/admin/controller/api/AdminApiController.java
+++ src/main/java/com/beelinkers/englebee/admin/controller/api/AdminApiController.java
@@ -1,8 +1,13 @@
 package com.beelinkers.englebee.admin.controller.api;
 
+import com.beelinkers.englebee.admin.dto.response.AdminDashboardMemberSubjectLevelCountDTO;
+import com.beelinkers.englebee.admin.dto.response.AdminMemberSubjectLevelCountDTO;
 import com.beelinkers.englebee.admin.service.AdminService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminApiController {
 
   private final AdminService adminService;
+
+  @GetMapping("/userlevel")
+  public ResponseEntity<AdminDashboardMemberSubjectLevelCountDTO> getChartUserTags() {
+    log.info("getChartUserTags");
+    List<AdminMemberSubjectLevelCountDTO> grammerList = adminService.getChartUserLevelGrammer();
+    List<AdminMemberSubjectLevelCountDTO> sentenceList = adminService.getChartUserLevelSentence();
+    List<AdminMemberSubjectLevelCountDTO> wordList = adminService.getChartUserLevelWord();
+    AdminDashboardMemberSubjectLevelCountDTO memberSubjectLevelCountDTO = new AdminDashboardMemberSubjectLevelCountDTO(grammerList,sentenceList,wordList);
+    return ResponseEntity.ok(memberSubjectLevelCountDTO);
+  }
 
 }
