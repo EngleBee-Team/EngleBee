b4b9939f02eca829ca8a700f8d713af27a1b703a
wonjun <wonjon0930@daum.net>
2024-08-29 08:09:09 +0900

    feat: LoginType Enum에 대해 OAuth2 Client Regitration 이름으로 부터 Enum 값을 가져올 수 있는 정적 팩토리 메서드 구현

--- src/main/java/com/beelinkers/englebee/auth/domain/entity/LoginType.java
+++ src/main/java/com/beelinkers/englebee/auth/domain/entity/LoginType.java
@@ -1,9 +1,32 @@
 package com.beelinkers.englebee.auth.domain.entity;
 
+import com.beelinkers.englebee.auth.exception.InvalidStudentGradeException;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum LoginType {
-  GOOGLE, NAVER
+  GOOGLE("google"),
+  NAVER("naver");
+
+  private final String alias;
+
+  public static LoginType fromAlias(String alias) {
+    for (LoginType loginType : values()) {
+      if (loginType.getAlias().equals(alias)) {
+        return loginType;
+      }
+    }
+    throw new InvalidStudentGradeException("유효하지 않은 로그인 타입 입력입니다: " + alias);
+  }
+
+  public boolean isGoogle() {
+    return this == GOOGLE;
+  }
+
+  public boolean isNaver() {
+    return this == NAVER;
+  }
 
 }
