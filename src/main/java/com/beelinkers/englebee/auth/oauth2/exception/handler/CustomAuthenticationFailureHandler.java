b5ceeefb59a57a16a6613bf6ed8cbf10ab6d6681
wonjun <wonjon0930@daum.net>
2024-08-29 08:34:17 +0900

    feat: AlreadySignedUpEmailException을 처리하기 위해 CustomAuthenticationFailureHandler에 예외 등록

--- src/main/java/com/beelinkers/englebee/auth/oauth2/exception/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/beelinkers/englebee/auth/oauth2/exception/handler/CustomAuthenticationFailureHandler.java
@@ -1,5 +1,6 @@
 package com.beelinkers.englebee.auth.oauth2.exception.handler;
 
+import com.beelinkers.englebee.auth.oauth2.exception.AlreadySignedUpEmailException;
 import com.beelinkers.englebee.auth.oauth2.exception.SignupRequiredException;
 import com.beelinkers.englebee.auth.oauth2.exception.UnsupportedSocialLoginTypeException;
 import jakarta.servlet.ServletException;
@@ -22,6 +23,10 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
       response.sendRedirect("/signup");
     } else if (exception instanceof UnsupportedSocialLoginTypeException) {
       // 구글, 네이버 이외의 소셜 로그인 타입
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().write(exception.getMessage());
+    } else if (exception instanceof AlreadySignedUpEmailException) {
+      // 이미 다른 소셜 로그인 타입으로 회원가입 완료된 경우
       response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
       response.getWriter().write(exception.getMessage());
     } else {
