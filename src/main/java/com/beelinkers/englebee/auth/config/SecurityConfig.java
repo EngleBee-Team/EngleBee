7ea0f644e711847302c53e782c38c6755d3c8fea
wonjun <wonjon0930@daum.net>
2024-08-29 09:00:46 +0900

    feat: Spring Security 동작 방식 설정을 위한 SecurityConfig 클래스 작성

---
+++ src/main/java/com/beelinkers/englebee/auth/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.beelinkers.englebee.auth.config;
+
+import com.beelinkers.englebee.auth.domain.entity.Role;
+import com.beelinkers.englebee.auth.oauth2.exception.handler.CustomAuthenticationFailureHandler;
+import com.beelinkers.englebee.auth.oauth2.userinfo.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final CustomOAuth2UserService customOAuth2UserService;
+  private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers("/h2-console/**"))
+        .headers(headers -> headers
+            .frameOptions(FrameOptionsConfig::sameOrigin));
+
+    http
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable);
+
+    // OAuth2 로그인 설정
+    http
+        .oauth2Login(oauth2 -> oauth2
+            .loginPage("/main")
+            .userInfoEndpoint(userInfoEndpointConfig ->
+                userInfoEndpointConfig.userService(customOAuth2UserService))
+            .failureHandler(customAuthenticationFailureHandler));
+
+    // 로그아웃 설정
+    http
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/main")
+            .deleteCookies("JSESSIONID")
+            .invalidateHttpSession(true));
+
+    // 접근 권한 설정
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/", "/oauth2/**", "/login/**", "/h2-console/**", "/api/auth/**",
+                "/main", "/signup")
+            .permitAll()
+            .requestMatchers("/api/admin/**").hasRole(Role.ADMIN.name())
+            .requestMatchers("/api/teacher/**").hasRole(Role.TEACHER.name())
+            .requestMatchers("/api/student/**").hasRole(Role.STUDENT.name())
+            .anyRequest().authenticated());
+
+    return http.build();
+  }
+}
