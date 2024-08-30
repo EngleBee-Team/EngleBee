871df149a4e067ba8bb3487979cd754b08d6a5f2
haram <rlagkfka9611@naver.com>
2024-08-30 16:51:42 +0900

    feat: 학생 나이대별 그룹 카운팅 Repository 메서드 및 sql문 구현

--- src/main/java/com/beelinkers/englebee/auth/domain/repository/MemberRepository.java
+++ src/main/java/com/beelinkers/englebee/auth/domain/repository/MemberRepository.java
@@ -1,8 +1,15 @@
 package com.beelinkers.englebee.auth.domain.repository;
 
+import com.beelinkers.englebee.admin.dto.response.AdminMemberAgeGroupCountDTO;
 import com.beelinkers.englebee.auth.domain.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+  @Query("SELECT new com.beelinkers.englebee.admin.dto.response.AdminMemberAgeGroupCountDTO(m.grade, COUNT(m)) " +
+      "FROM Member m " +
+      "GROUP BY m.grade")
+  List<AdminMemberAgeGroupCountDTO> findAgeGroupCount();
 }
