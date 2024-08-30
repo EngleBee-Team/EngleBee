bfbf5855425994ae6817c68c3681aba5829d7569
haram <rlagkfka9611@naver.com>
2024-08-30 16:37:06 +0900

    feat: llm 호출수, 수업 생성 카운트 차트 생성 컨트롤러 메서드 구현

--- src/main/java/com/beelinkers/englebee/admin/controller/api/AdminApiController.java
+++ src/main/java/com/beelinkers/englebee/admin/controller/api/AdminApiController.java
@@ -1,6 +1,9 @@
 package com.beelinkers.englebee.admin.controller.api;
 
+import com.beelinkers.englebee.admin.dto.request.AdminYearMonthDTO;
+import com.beelinkers.englebee.admin.dto.response.AdminDashboardDayGroupCountDTO;
 import com.beelinkers.englebee.admin.dto.response.AdminDashboardMemberSubjectLevelCountDTO;
+import com.beelinkers.englebee.admin.dto.response.AdminMemberAgeGroupCountDTO;
 import com.beelinkers.englebee.admin.dto.response.AdminMemberSubjectLevelCountDTO;
 import com.beelinkers.englebee.admin.service.AdminService;
 import java.util.List;
@@ -8,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +24,30 @@ public class AdminApiController {
 
   private final AdminService adminService;
 
-  @GetMapping("/userlevel")
+  @GetMapping("/member-chart")
   public ResponseEntity<AdminDashboardMemberSubjectLevelCountDTO> getChartUserTags() {
-    log.info("getChartUserTags");
     List<AdminMemberSubjectLevelCountDTO> grammerList = adminService.getChartUserLevelGrammer();
     List<AdminMemberSubjectLevelCountDTO> sentenceList = adminService.getChartUserLevelSentence();
     List<AdminMemberSubjectLevelCountDTO> wordList = adminService.getChartUserLevelWord();
-    AdminDashboardMemberSubjectLevelCountDTO memberSubjectLevelCountDTO = new AdminDashboardMemberSubjectLevelCountDTO(grammerList,sentenceList,wordList);
+    List<AdminMemberAgeGroupCountDTO> ageList = adminService.getChartUserAgeGroup();
+    AdminDashboardMemberSubjectLevelCountDTO memberSubjectLevelCountDTO = new AdminDashboardMemberSubjectLevelCountDTO(grammerList,sentenceList,wordList,ageList);
     return ResponseEntity.ok(memberSubjectLevelCountDTO);
+  }
+
+  @PostMapping("/call-llm")
+  public ResponseEntity<List<AdminDashboardDayGroupCountDTO>> getChartCallLLM(@RequestBody AdminYearMonthDTO adminYearMonthDTO) {
+    int year = Integer.parseInt(adminYearMonthDTO.getYear());
+    int month = Integer.parseInt(adminYearMonthDTO.getMonth());
+    List<AdminDashboardDayGroupCountDTO> llmList = adminService.getChartDayCallLLMCount(year,month);
+    return ResponseEntity.ok(llmList);
+  }
+
+  @PostMapping("/create-lecture")
+  public ResponseEntity<List<AdminDashboardDayGroupCountDTO>> getChartCreateLecture(@RequestBody AdminYearMonthDTO adminYearMonthDTO) {
+    int year = Integer.parseInt(adminYearMonthDTO.getYear());
+    int month = Integer.parseInt(adminYearMonthDTO.getMonth());
+    List<AdminDashboardDayGroupCountDTO> lectureList = adminService.getChartDayCreateLectureCount(year,month);
+    return ResponseEntity.ok(lectureList);
   }
 
 }
