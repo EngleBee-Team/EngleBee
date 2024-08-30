d0103b69f1f66499cabfcae7e925a68550ffab35
haram <rlagkfka9611@naver.com>
2024-08-30 16:38:08 +0900

    feat: llm 호출수, 수업 생성, 학생 나이대별 그룹 카운팅 차트 생성 서비스 메서드 구현

--- src/main/java/com/beelinkers/englebee/admin/service/AdminService.java
+++ src/main/java/com/beelinkers/englebee/admin/service/AdminService.java
@@ -1,5 +1,7 @@
 package com.beelinkers.englebee.admin.service;
 
+import com.beelinkers.englebee.admin.dto.response.AdminDashboardDayGroupCountDTO;
+import com.beelinkers.englebee.admin.dto.response.AdminMemberAgeGroupCountDTO;
 import com.beelinkers.englebee.admin.dto.response.AdminMemberSubjectLevelCountDTO;
 import java.util.List;
 
@@ -10,4 +12,10 @@ public interface AdminService {
   List<AdminMemberSubjectLevelCountDTO> getChartUserLevelSentence();
 
   List<AdminMemberSubjectLevelCountDTO> getChartUserLevelWord();
+
+  List<AdminDashboardDayGroupCountDTO> getChartDayCallLLMCount(int year, int month);
+
+  List<AdminMemberAgeGroupCountDTO> getChartUserAgeGroup();
+
+  List<AdminDashboardDayGroupCountDTO> getChartDayCreateLectureCount(int year, int month);
 }
--- src/main/java/com/beelinkers/englebee/admin/service/impl/AdminServiceImpl.java
+++ src/main/java/com/beelinkers/englebee/admin/service/impl/AdminServiceImpl.java
@@ -1,9 +1,15 @@
 package com.beelinkers.englebee.admin.service.impl;
 
+import com.beelinkers.englebee.admin.domain.repository.CallLLMLogRepository;
+import com.beelinkers.englebee.admin.domain.repository.CreatedLectureLogRepository;
+import com.beelinkers.englebee.admin.dto.response.AdminDashboardDayGroupCountDTO;
+import com.beelinkers.englebee.admin.dto.response.AdminMemberAgeGroupCountDTO;
 import com.beelinkers.englebee.admin.dto.response.AdminMemberSubjectLevelCountDTO;
 import com.beelinkers.englebee.admin.service.AdminService;
+import com.beelinkers.englebee.auth.domain.repository.MemberRepository;
 import com.beelinkers.englebee.general.domain.repository.MemberSubjectLevelRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +20,9 @@ import org.springframework.stereotype.Service;
 public class AdminServiceImpl implements AdminService {
 
   private final MemberSubjectLevelRepository memberSubjectLevelRepository;
+  private final CallLLMLogRepository callLLMLogRepository;
+  private final MemberRepository memberRepository;
+  private final CreatedLectureLogRepository createdLectureLogRepository;
 
   @Override
   public List<AdminMemberSubjectLevelCountDTO> getChartUserLevelGrammer() {
@@ -30,5 +39,27 @@ public class AdminServiceImpl implements AdminService {
     return memberSubjectLevelRepository.findLevelCountsWord();
   }
 
+  @Override
+  public List<AdminDashboardDayGroupCountDTO> getChartDayCallLLMCount(int year,
+      int month) {
+    List<Object[]> results = callLLMLogRepository.findCallLLMGroupByDay(year, month);
+    return results.stream()
+        .map(result -> new AdminDashboardDayGroupCountDTO(((Number) result[0]).intValue(), ((Number) result[1]).longValue()))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public List<AdminMemberAgeGroupCountDTO> getChartUserAgeGroup() {
+    List<AdminMemberAgeGroupCountDTO> ageGroupList = memberRepository.findAgeGroupCount();
+    return ageGroupList;
+  }
+
+  @Override
+  public List<AdminDashboardDayGroupCountDTO> getChartDayCreateLectureCount(int year, int month) {
+    List<Object[]> results = createdLectureLogRepository.findCreateLectureGroupByDay(year, month);
+    return results.stream()
+        .map(result -> new AdminDashboardDayGroupCountDTO(((Number) result[0]).intValue(), ((Number) result[1]).longValue()))
+        .collect(Collectors.toList());
+  }
 
 }
