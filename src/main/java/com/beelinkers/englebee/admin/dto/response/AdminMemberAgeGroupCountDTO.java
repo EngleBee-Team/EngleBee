e4caf342aa1d256491bdb3cd784d39e0b0fe3caa
haram <rlagkfka9611@naver.com>
2024-08-30 21:10:13 +0900

    chore: 사용안하는 Builder 에노테이션 삭제

--- src/main/java/com/beelinkers/englebee/admin/dto/response/AdminMemberAgeGroupCountDTO.java
+++ src/main/java/com/beelinkers/englebee/admin/dto/response/AdminMemberAgeGroupCountDTO.java
@@ -2,12 +2,10 @@ package com.beelinkers.englebee.admin.dto.response;
 
 import com.beelinkers.englebee.auth.domain.entity.StudentGrade;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminMemberAgeGroupCountDTO {
