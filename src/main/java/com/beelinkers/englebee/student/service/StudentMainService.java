53fe1d92433a7bdc6d801d75a10740913525962f
yeong1203 <167217894+yeong1203@users.noreply.github.com>
2024-09-01 12:19:24 +0900

    [refactor-student-main-api] 학생 메인 페이지 api 코드 수정 및 Test Code 구현 (#32)
    * chore: 학생 메인 페이지 QnA 리스트 불필요하여 코드 삭제
    
    * feat: 학생 메인 페이지 lecture 목록 subjectLevelCode DTO 구현
    
    * chore: 학생 메인 페이지 Lecture 목록에서 N+1 문제로 코드 수정
    
    * chore: 학생 메인 페이지 exam 목록 최신글 TOP5 조회 코드 수정
    
    * chore: 학생 메인 페이지 lecture 조회 코드 수정
    
    * chore: 학생 메인 페이지 메서드 네이밍 수정
    
    * chore: 학생 메인 페이지 api 불필요한 코드 삭제
    
    * feat: 학생 메인 페이지 api 컨트롤러 & 서비스 테스트코드 작성.
    
    * chore: 학생 메인 페이지 api 관련 메서드명 수정
    
    * chore: 학생 메인 페이지 테스트 코드 api 관련 메서드명 수정

--- src/main/java/com/beelinkers/englebee/student/service/StudentMainService.java
+++ src/main/java/com/beelinkers/englebee/student/service/StudentMainService.java
@@ -1,13 +1,20 @@
 package com.beelinkers.englebee.student.service;
 
-import com.beelinkers.englebee.student.dto.response.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.beelinkers.englebee.general.domain.entity.ExamStatus;
+import com.beelinkers.englebee.general.domain.entity.LectureStatus;
+import com.beelinkers.englebee.student.dto.response.StudentMainPageLectureDTO;
+import com.beelinkers.englebee.student.dto.response.StudentMainPageNewExamDTO;
+import com.beelinkers.englebee.student.dto.response.StudentMainPageSubmitExamDTO;
+import java.util.List;
 
 public interface StudentMainService {
-    Page<StudentMainPageLectureDTO> getLectureList(Long memberSeq, Pageable pageable);
-    Page<StudentMainPageQuestionDTO> getQuestionList(Pageable pageable);
-    Page<StudentMainPageNewExamDTO> getNewExamList(Long memberSeq, Pageable pageable);
-    Page<StudentMainPageSubmitExamDTO> getSubmitExamList(Long memberSeq, Pageable pageable);
+
+  List<StudentMainPageLectureDTO> getOngoingLectureInfo(Long memberSeq, Long lectureSeq,
+      LectureStatus lectureStatus);
+
+  List<StudentMainPageNewExamDTO> getPreparedExamInfo(Long memberSeq, ExamStatus status);
+
+  List<StudentMainPageSubmitExamDTO> getCompletedExamInfo(Long memberSeq,
+      List<ExamStatus> status);
 
 }
