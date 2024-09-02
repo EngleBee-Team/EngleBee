61f0fe8bb86c20b9225c778fd14e0a591e9ba0b2
wonjun <wonjon0930@daum.net>
2024-09-02 10:09:52 +0900

    chore: @SpringBootApplication에서 시큐리티 및 OAuth2 로그인 설정 제외 관련 주석 제거

--- src/main/java/com/beelinkers/englebee/EngleBeeApplication.java
+++ src/main/java/com/beelinkers/englebee/EngleBeeApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication/*(exclude = {SecurityAutoConfiguration.class,
-    OAuth2ClientAutoConfiguration.class,})*/
+@SpringBootApplication
 public class EngleBeeApplication {
 
   public static void main(String[] args) {
