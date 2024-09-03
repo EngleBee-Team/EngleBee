03b5a8d8d11baf5208da4c80cfe81a64521c52aa
yeong1203 <167217894+yeong1203@users.noreply.github.com>
2024-09-03 18:43:28 +0900

    [feat-student-account-all] 학생 계정 페이지  api 구현 및 공통 Account Page Controller 및 service 구현 (#47)
    * feat: Member 엔티티에 닉네임 및 학년 수정 메서드 추가
    
    * feat: 학생 계정 페이지 api 관련 DTO 및 Mapper 구현
    
    * feat: 학생 계정 페이지 api 관련 service 구현
    
    * feat: 학생 계정 페이지 api 관련 controller 구현
    
    * feat: (공통) 계정 페이지 관련 service 구현
    
    * feat: (공통) 계정 페이지 관련 view 기본 구현
    
    * feat: (공통) 계정 페이지 관련 Controller 구현
    
    * chore: 불필요한 파일 삭제
    
    * chore: 학생 계정 페이지 api 관련 DTO 네이밍 수정
    
    * chore: 불필요한 코드 삭제 및 변경된 DTO 네이밍 적용

--- src/main/java/com/beelinkers/englebee/student/service/StudentAccountService.java
+++ src/main/java/com/beelinkers/englebee/student/service/StudentAccountService.java
@@ -1,5 +1,16 @@
 package com.beelinkers.englebee.student.service;
 
+import com.beelinkers.englebee.auth.domain.entity.Member;
+import com.beelinkers.englebee.student.dto.request.StudentAccountPageRequestDTO;
+
 public interface StudentAccountService {
 
+  Member updateStudentInfo(Long memberSeq,
+      StudentAccountPageRequestDTO studentAccountPageUpdateRequestDTO);
+
+  void deleteStudentAccountInfo(Long memberSeq);
+
+  boolean checkNicknameDuplicated(String nickname);
+
+  Member getMemberInfo(Long memberSeq);
 }
