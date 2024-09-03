46583009275bddd64e940a88aa859f2ed4fbde0e
yeong1203 <cozyluv117@gmail.com>
2024-09-03 21:13:53 +0900

    feat: Q&A 리스트 페이지 api 관련 service 구현

--- src/main/java/com/beelinkers/englebee/general/service/QnaService.java
+++ src/main/java/com/beelinkers/englebee/general/service/QnaService.java
@@ -1,5 +1,10 @@
 package com.beelinkers.englebee.general.service;
 
+import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface QnaService {
 
+  Page<QnaPageResponseDTO> getQnaList(Pageable pageable);
 }
--- src/main/java/com/beelinkers/englebee/general/service/impl/QnaServiceImpl.java
+++ src/main/java/com/beelinkers/englebee/general/service/impl/QnaServiceImpl.java
@@ -1,11 +1,30 @@
 package com.beelinkers.englebee.general.service.impl;
 
+import com.beelinkers.englebee.general.domain.repository.QuestionRepository;
+import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
+import com.beelinkers.englebee.general.dto.response.mapper.QnaPageMapper;
 import com.beelinkers.englebee.general.service.QnaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class QnaServiceImpl implements QnaService {
+
+  private final QuestionRepository questionRepository;
+  private final QnaPageMapper qnaPageMapper;
+
+  @Override
+  @Transactional(readOnly = true)
+  public Page<QnaPageResponseDTO> getQnaList(Pageable pageable) {
+    return questionRepository.findAllByOrderByCreatedAtDesc(pageable)
+        .map(qnaPageMapper::qnaPageResponseDTO);
+  }
 
 }
