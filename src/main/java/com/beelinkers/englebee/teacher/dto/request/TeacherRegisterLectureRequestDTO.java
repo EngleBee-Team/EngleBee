a51562b75b2d1ee15870d6a4dd7260a73415b880
haram <rlagkfka9611@naver.com>
2024-09-03 21:04:23 +0900

    refactor: 수업, 레벨 담은 DTO를 list로 관리

--- src/main/java/com/beelinkers/englebee/teacher/dto/request/TeacherRegisterLectureRequestDTO.java
+++ src/main/java/com/beelinkers/englebee/teacher/dto/request/TeacherRegisterLectureRequestDTO.java
@@ -1,5 +1,6 @@
 package com.beelinkers.englebee.teacher.dto.request;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,6 @@ public class TeacherRegisterLectureRequestDTO {
 
   private String studentNickname;
   private String letureTitle;
-  private String subject;
-  private String level;
+  private List<TeacherRegisterLectureSubjectLevelDTO> subjectLevels;
+
 }
