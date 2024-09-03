bdf30c50868375021690076c7bca45f86f92cf88
yeong1203 <cozyluv117@gmail.com>
2024-09-03 21:32:03 +0900

    feat: Q&A 리스트 페이지 api 관련 controller 구현

--- src/main/java/com/beelinkers/englebee/general/controller/api/QnaApiController.java
+++ src/main/java/com/beelinkers/englebee/general/controller/api/QnaApiController.java
@@ -1,8 +1,16 @@
 package com.beelinkers.englebee.general.controller.api;
 
+import com.beelinkers.englebee.general.dto.response.GeneralPagedResponseDTO;
+import com.beelinkers.englebee.general.dto.response.PaginationResponseDTO;
+import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
 import com.beelinkers.englebee.general.service.QnaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class QnaApiController {
 
   private final QnaService qnaService;
+
+  @GetMapping("/list")
+  public ResponseEntity<GeneralPagedResponseDTO<QnaPageResponseDTO>> getQnaList(
+      @PageableDefault(size = 10) Pageable pageable
+  ) {
+    Page<QnaPageResponseDTO> QnaInfoList = qnaService.getQnaListInfo(pageable);
+    PaginationResponseDTO pagination = new PaginationResponseDTO(
+        QnaInfoList.getNumber(), QnaInfoList.getSize(), QnaInfoList.getTotalPages(),
+        QnaInfoList.getTotalElements(), QnaInfoList.hasPrevious(), QnaInfoList.hasNext()
+    );
+    GeneralPagedResponseDTO<QnaPageResponseDTO> resultQna = new GeneralPagedResponseDTO<>(
+        QnaInfoList.getContent(), pagination
+    );
+
+    return ResponseEntity.ok(resultQna);
+  }
 }
