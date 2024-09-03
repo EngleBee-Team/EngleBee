265213cf9cd073b59d4da4ca0b8aabd4b927af0f
yeong1203 <cozyluv117@gmail.com>
2024-09-03 12:04:47 +0900

    feat: 학생 계정 페이지 api 관련 service 구현

--- src/main/java/com/beelinkers/englebee/student/service/StudentAccountService.java
+++ src/main/java/com/beelinkers/englebee/student/service/StudentAccountService.java
@@ -1,5 +1,16 @@
 package com.beelinkers.englebee.student.service;
 
+import com.beelinkers.englebee.auth.domain.entity.Member;
+import com.beelinkers.englebee.student.dto.request.StudentAccountPageUpdateRequestDTO;
+
 public interface StudentAccountService {
 
+  Member updateStudentInfo(Long memberSeq,
+      StudentAccountPageUpdateRequestDTO studentAccountPageUpdateRequestDTO);
+
+  void deleteStudentAccountInfo(Long memberSeq);
+
+  boolean checkNicknameDuplicated(String nickname);
+
+  Member getMemberInfo(Long memberSeq);
 }
--- src/main/java/com/beelinkers/englebee/student/service/impl/StudentAccountServiceImpl.java
+++ src/main/java/com/beelinkers/englebee/student/service/impl/StudentAccountServiceImpl.java
@@ -1,11 +1,65 @@
 package com.beelinkers.englebee.student.service.impl;
 
+import com.beelinkers.englebee.auth.domain.entity.Member;
+import com.beelinkers.englebee.auth.domain.entity.StudentGrade;
+import com.beelinkers.englebee.auth.domain.repository.MemberRepository;
+import com.beelinkers.englebee.general.exception.MemberNotFoundException;
+import com.beelinkers.englebee.general.service.GeneralMemberService;
+import com.beelinkers.englebee.student.dto.request.StudentAccountPageUpdateRequestDTO;
 import com.beelinkers.englebee.student.service.StudentAccountService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class StudentAccountServiceImpl implements StudentAccountService {
 
+  private final GeneralMemberService generalMemberService;
+  private final MemberRepository memberRepository;
+
+  // 회원 정보
+  @Override
+  @Transactional(readOnly = true)
+  public Member getMemberInfo(Long memberSeq) {
+    return memberRepository.findById(memberSeq)
+        .orElseThrow(() -> new MemberNotFoundException("해당 회원을 찾을 수 없습니다."));
+  }
+
+  // 닉네임 중복 검사
+  @Override
+  @Transactional
+  public boolean checkNicknameDuplicated(String nickname) {
+    return generalMemberService.checkNicknameDuplicated(nickname);
+  }
+
+  // 회원 정보 수정
+  @Override
+  @Transactional
+  public Member updateStudentInfo(Long memberSeq,
+      StudentAccountPageUpdateRequestDTO updateStudentRequest) {
+    Member member = memberRepository.findById(memberSeq)
+        .orElseThrow(() -> new RuntimeException("해당 회원을 찾을 수 없습니다."));
+
+    member.updateNickname(updateStudentRequest.getNickname());
+    member.updateStudentGrade(StudentGrade.fromKoreanGrade(updateStudentRequest.getGrade()));
+
+    return memberRepository.save(member);
+  }
+
+  // 회원 탈퇴
+  @Override
+  @Transactional
+  public void deleteStudentAccountInfo(Long memberSeq) {
+    Member member = memberRepository.findById(memberSeq)
+        .orElseThrow(() -> new MemberNotFoundException("해당 유저가 존재하지 않습니다."));
+    if (!member.isDeactivated()) {
+      member.deactivate();
+    }
+  }
+
+  ;
 }
