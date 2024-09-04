04b7164404e6150fffc4a1516743f165648c3a0f
yeong1203 <cozyluv117@gmail.com>
2024-09-04 10:55:40 +0900

    chore: Qna 글 등록 페이지 DTO 수정

--- src/main/java/com/beelinkers/englebee/general/dto/request/QnaPageRequestDTO.java
+++ src/main/java/com/beelinkers/englebee/general/dto/request/QnaPageRequestDTO.java
@@ -1,6 +1,5 @@
 package com.beelinkers.englebee.general.dto.request;
 
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,5 @@ public class QnaPageRequestDTO {
   private Long memberSeq;
   private String title;
   private String content;
-  private LocalDateTime createdAt;
 
 }
