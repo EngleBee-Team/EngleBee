a8e1715d0538712bae486847bfb63e139cc7b7a6
yeong1203 <cozyluv117@gmail.com>
2024-09-04 12:26:24 +0900

    feat: Q&A 상세 페이지 내 댓글 등록 service 구현

--- src/main/java/com/beelinkers/englebee/general/service/QnaService.java
+++ src/main/java/com/beelinkers/englebee/general/service/QnaService.java
@@ -1,7 +1,9 @@
 package com.beelinkers.englebee.general.service;
 
 import com.beelinkers.englebee.general.dto.request.QnaPageRequestDTO;
+import com.beelinkers.englebee.general.dto.request.ReplyRequestDTO;
 import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
+import com.beelinkers.englebee.general.dto.response.ReplyResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,5 +13,6 @@ public interface QnaService {
 
   Page<QnaPageResponseDTO> getQnaListInfo(Pageable pageable);
 
+  ReplyResponseDTO registerReplyInfo(ReplyRequestDTO replyRequestDTO);
 
 }
--- src/main/java/com/beelinkers/englebee/general/service/impl/QnaServiceImpl.java
+++ src/main/java/com/beelinkers/englebee/general/service/impl/QnaServiceImpl.java
@@ -3,11 +3,17 @@ package com.beelinkers.englebee.general.service.impl;
 import com.beelinkers.englebee.auth.domain.entity.Member;
 import com.beelinkers.englebee.auth.domain.repository.MemberRepository;
 import com.beelinkers.englebee.general.domain.entity.Question;
+import com.beelinkers.englebee.general.domain.entity.Reply;
 import com.beelinkers.englebee.general.domain.repository.QuestionRepository;
+import com.beelinkers.englebee.general.domain.repository.ReplyRepository;
 import com.beelinkers.englebee.general.dto.request.QnaPageRequestDTO;
+import com.beelinkers.englebee.general.dto.request.ReplyRequestDTO;
 import com.beelinkers.englebee.general.dto.request.mapper.QnaPageRequestMapper;
+import com.beelinkers.englebee.general.dto.request.mapper.ReplyRequestMapper;
 import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
+import com.beelinkers.englebee.general.dto.response.ReplyResponseDTO;
 import com.beelinkers.englebee.general.dto.response.mapper.QnaPageResponseMapper;
+import com.beelinkers.englebee.general.dto.response.mapper.ReplyResponseMapper;
 import com.beelinkers.englebee.general.service.QnaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +31,10 @@ public class QnaServiceImpl implements QnaService {
   private final QuestionRepository questionRepository;
   private final QnaPageResponseMapper qnaPageMapper;
   private final MemberRepository memberRepository;
+  private final ReplyRepository replyRepository;
   private final QnaPageRequestMapper qnaPageRequestMapper;
+  private final ReplyRequestMapper replyRequestMapper;
+  private final ReplyResponseMapper replyResponseMapper;
 
   @Override
   @Transactional
@@ -41,6 +50,19 @@ public class QnaServiceImpl implements QnaService {
   public Page<QnaPageResponseDTO> getQnaListInfo(Pageable pageable) {
     return questionRepository.findAllByOrderByCreatedAtDesc(pageable)
         .map(qnaPageMapper::qnaPageResponseDTO);
+  }
+
+  @Override
+  @Transactional
+  public ReplyResponseDTO registerReplyInfo(ReplyRequestDTO replyRequestDTO) {
+    Question question = questionRepository.findById(replyRequestDTO.getQuestionSeq())
+        .orElseThrow(() -> new IllegalArgumentException("질문을 찾을 수 없습니다."));
+    Member member = memberRepository.findById(replyRequestDTO.getMemberSeq())
+        .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+    Reply reply = replyRequestMapper.registerReply(replyRequestDTO, question, member);
+    Reply saveReply = replyRepository.save(reply);
+
+    return replyResponseMapper.replyResponseDTO(saveReply);
   }
 
 }
