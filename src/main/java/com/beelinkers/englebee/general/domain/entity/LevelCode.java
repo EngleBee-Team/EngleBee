d68b9f69a57c2b0fb5e3be4e7e7ba013faa93be7
wonjun <wonjon0930@daum.net>
2024-09-04 09:55:25 +0900

    fix: LevelCode Enum에 필요하지 않은 어노테이션, Getter 메서드 제거

--- src/main/java/com/beelinkers/englebee/general/domain/entity/LevelCode.java
+++ src/main/java/com/beelinkers/englebee/general/domain/entity/LevelCode.java
@@ -1,8 +1,6 @@
 package com.beelinkers.englebee.general.domain.entity;
 
 import com.beelinkers.englebee.general.exception.InvalidLevelCodeException;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +16,6 @@ public enum LevelCode {
 
   private final String koreanCode;
 
-  @JsonCreator
   public static LevelCode fromKoreanCode(String koreanCode) {
     for (LevelCode level : values()) {
       if (level.getKoreanCode().equals(koreanCode)) {
@@ -28,9 +25,8 @@ public enum LevelCode {
     throw new InvalidLevelCodeException("유효하지 않은 레벨 입력입니다: " + koreanCode);
   }
 
-  @JsonValue
   public String getKoreanCode() {
     return koreanCode;
   }
-
+  
 }
