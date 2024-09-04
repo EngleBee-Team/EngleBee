30699a326903f27f15d58138183e7b0d3a12aa4d
yeong1203 <cozyluv117@gmail.com>
2024-09-04 10:32:25 +0900

    feat: Q&A 글 등록 페이지 service 구현

--- src/main/java/com/beelinkers/englebee/general/service/QnaService.java
+++ src/main/java/com/beelinkers/englebee/general/service/QnaService.java
@@ -1,10 +1,14 @@
 package com.beelinkers.englebee.general.service;
 
+import com.beelinkers.englebee.general.domain.entity.Question;
+import com.beelinkers.englebee.general.dto.request.QnaPageRequestDTO;
 import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface QnaService {
+
+  Question registerQuestion(QnaPageRequestDTO qnaRequestDTO, Long memberSeq);
 
   Page<QnaPageResponseDTO> getQnaListInfo(Pageable pageable);
 }
--- src/main/java/com/beelinkers/englebee/general/service/impl/QnaServiceImpl.java
+++ src/main/java/com/beelinkers/englebee/general/service/impl/QnaServiceImpl.java
@@ -1,6 +1,11 @@
 package com.beelinkers.englebee.general.service.impl;
 
+import com.beelinkers.englebee.auth.domain.entity.Member;
+import com.beelinkers.englebee.auth.domain.repository.MemberRepository;
+import com.beelinkers.englebee.general.domain.entity.Question;
 import com.beelinkers.englebee.general.domain.repository.QuestionRepository;
+import com.beelinkers.englebee.general.dto.request.QnaPageRequestDTO;
+import com.beelinkers.englebee.general.dto.request.mapper.QnaPageRequestMapper;
 import com.beelinkers.englebee.general.dto.response.QnaPageResponseDTO;
 import com.beelinkers.englebee.general.dto.response.mapper.QnaPageResponseMapper;
 import com.beelinkers.englebee.general.service.QnaService;
@@ -19,6 +24,17 @@ public class QnaServiceImpl implements QnaService {
 
   private final QuestionRepository questionRepository;
   private final QnaPageResponseMapper qnaPageMapper;
+  private final MemberRepository memberRepository;
+  private final QnaPageRequestMapper qnaPageRequestMapper;
+
+  @Override
+  @Transactional
+  public Question registerQuestion(QnaPageRequestDTO qnaRequestDTO, Long memberSeq) {
+    Member member = memberRepository.findById(memberSeq)
+        .orElseThrow(() -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다."));
+    Question questionRegister = qnaPageRequestMapper.registerQnaDTO(qnaRequestDTO, member);
+    return questionRepository.save(questionRegister);
+  }
 
   @Override
   @Transactional(readOnly = true)
