4a26c88f35491a7413f57cc8028480c59b8754e1
wonjun <wonjon0930@daum.net>
2024-09-05 11:42:31 +0900

    feat: GPT와의 비동기 통신 작업을 위한 쓰레드 풀 설정 추가

---
+++ src/main/java/com/beelinkers/englebee/global/GptAsyncConfig.java
@@ -0,0 +1,22 @@
+package com.beelinkers.englebee.global;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class GptAsyncConfig {
+
+  @Bean(name = "gptThreadPoolTask")
+  public Executor gptThreadPoolTask() {
+    ThreadPoolTaskExecutor poolExecutor = new ThreadPoolTaskExecutor();
+    poolExecutor.setCorePoolSize(10);
+    poolExecutor.setMaxPoolSize(100);
+    poolExecutor.setQueueCapacity(100);
+    poolExecutor.setThreadNamePrefix("Gpt-");
+    return poolExecutor;
+  }
+}
