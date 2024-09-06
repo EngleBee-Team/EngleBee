1100c625c192461e143548d27e2947c179f03e1c
haram <rlagkfka9611@naver.com>
2024-09-06 12:10:28 +0900

    feat: 수업 등록 js 로직 생성

---
+++ src/main/resources/static/js/register-lecture.js
@@ -0,0 +1,63 @@
+$(function(){
+
+
+
+  $("#register-lecture-button").click(function(){
+    let wordLevel = $("#wordLevel").val();
+    let senetenceLevel = $("#sentenceLevel").val();
+    let grammerLevel = $("#grammerLevel").val();
+    let studentNickname = $("#student-nickname").val();
+    let lectureTitle = $("#lecture-title").val();
+
+    let subjectLevels = [
+      {
+        "subject": "단어",
+        "level": wordLevel
+      },
+      {
+        "subject": "문장",
+        "level": senetenceLevel
+      },
+      {
+        "subject": "어법",
+        "level": grammerLevel
+      }
+    ];
+
+    let data = JSON.stringify({
+      "studentNickname": studentNickname,
+      "letureTitle" : lectureTitle,
+      "subjectLevels" : subjectLevels
+    })
+
+    if(!studentNickname){
+      alert("학생 닉네임을 입력하세요");
+    }else if(!lectureTitle){
+      alert("수업명을 입력하세요");
+    }else{
+
+      $.ajax({
+        url: '/api/teacher/lecture/register',
+        method: 'POST',
+        contentType: "application/json",
+        data : data
+        ,
+        success: function (data) {
+          console.log("성공");
+          alert("수업 생성되었습니다")
+        },
+
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.error('[ERROR] 데이터를 불러오던 도중 에러가 발생했습니다');
+          alert(jqXHR.responseText);
+        }
+      });
+
+    }
+
+
+
+  })
+
+
+})
