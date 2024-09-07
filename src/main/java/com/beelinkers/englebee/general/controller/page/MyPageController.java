1ee27011ae6efa582760b5abc895f4255555cdc1
yeong1203 <167217894+yeong1203@users.noreply.github.com>
2024-09-07 20:22:54 +0900

    [feat-teacher-my-view] 선생님 my 페이지에서 내가 작성한 게시글 리스트 조회 기능 구현 (#74)
    * chore: 불필요한 코드 삭제
    
    * feat: 선생님 my 페이지 view 관련 controller 구현
    
    * feat: 선생님 my 페이지 view 구현
    
    * fix: 페이징 처리 중 페이지 넘길 때 오류 발생으로 인해 코드 수정.
    
    * feat: 선생님 my 페이지 view 구현 완료
    
    * fix: 학생 my 페이지 view 페이지네이션 오류 발생으로 코드 수정
    
    * chore: 불필요한 코드 삭제 및 페이지네이션 관련 코드 수정
    
    * chore: 페이지네이션 관련 코드 수정

--- src/main/java/com/beelinkers/englebee/general/controller/page/MyPageController.java
+++ src/main/java/com/beelinkers/englebee/general/controller/page/MyPageController.java
@@ -10,16 +10,22 @@ import com.beelinkers.englebee.student.dto.response.StudentMyPageCreatedExamDTO;
 import com.beelinkers.englebee.student.dto.response.StudentMyPageWrittenQnaDTO;
 import com.beelinkers.englebee.student.dto.response.StudentMyPageWrittenReplyDTO;
 import com.beelinkers.englebee.student.service.StudentMyService;
+import com.beelinkers.englebee.teacher.dto.response.TeacherMyPageExamHistoryDTO;
+import com.beelinkers.englebee.teacher.dto.response.TeacherMyPagePendingExamDTO;
+import com.beelinkers.englebee.teacher.dto.response.TeacherMyPageWrittenQnaDTO;
+import com.beelinkers.englebee.teacher.dto.response.TeacherMyPageWrittenReplyDTO;
 import com.beelinkers.englebee.teacher.service.TeacherMyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -33,8 +39,14 @@ public class MyPageController {
   private final TeacherMyService teacherMyService;
 
   @GetMapping("/info")
-  public String getMyInfoPage(@LoginMember SessionMember sessionMember, Model model,
-      @PageableDefault(size = 10) Pageable pageable) {
+  public String getMyInfoPage(
+      @LoginMember SessionMember sessionMember,
+      Model model,
+      @PageableDefault(size = 10) Pageable pageable,
+      @RequestParam(value = "pagePending", defaultValue = "0") int pagePending,
+      @RequestParam(value = "pageCreated", defaultValue = "0") int pageCreated,
+      @RequestParam(value = "pageQna", defaultValue = "0") int pageQna,
+      @RequestParam(value = "pageReply", defaultValue = "0") int pageReply) {
 
     Long memberSeq = sessionMember.getSeq();
     model.addAttribute("nickname", myPageService.getNickname(memberSeq));
@@ -43,22 +55,25 @@ public class MyPageController {
     Role sessionUserRole = sessionMember.getRole();
     if (sessionUserRole.isStudent()) {
 
-      Page<StudentMyPageCreatedExamDTO> createdExamList = studentMyService.getStudentMyCreatedExamInfo(
-          memberSeq, pageable);
-      Page<StudentMyPageCompletedExamDTO> completedExamList = studentMyService.getStudentMyCompletedExamInfo(
-          memberSeq, pageable);
-      Page<StudentMyPageWrittenQnaDTO> writtenQnaList = studentMyService.getStudentMyWrittenQnaInfo(
-          memberSeq, pageable);
-      Page<StudentMyPageWrittenReplyDTO> writtenReplyList = studentMyService.getStudentMyWrittenReplyInfo(
-          memberSeq, pageable);
+      Pageable pendingPageable = PageRequest.of(pagePending, pageable.getPageSize());
+      Pageable createdPageable = PageRequest.of(pageCreated, pageable.getPageSize());
+      Pageable qnaPageable = PageRequest.of(pageQna, pageable.getPageSize());
+      Pageable replyPageable = PageRequest.of(pageReply, pageable.getPageSize());
 
-      // Pagination 정보 생성 및 모델에 추가
+      Page<StudentMyPageCreatedExamDTO> createdExamList = studentMyService.getStudentMyCreatedExamInfo(
+          memberSeq, createdPageable);
+      Page<StudentMyPageCompletedExamDTO> completedExamList = studentMyService.getStudentMyCompletedExamInfo(
+          memberSeq, pendingPageable);
+      Page<StudentMyPageWrittenQnaDTO> writtenQnaList = studentMyService.getStudentMyWrittenQnaInfo(
+          memberSeq, qnaPageable);
+      Page<StudentMyPageWrittenReplyDTO> writtenReplyList = studentMyService.getStudentMyWrittenReplyInfo(
+          memberSeq, replyPageable);
+
       model.addAttribute("createdExamsPagination", paginationResponse(createdExamList));
       model.addAttribute("completedExamsPagination", paginationResponse(completedExamList));
       model.addAttribute("writtenQnaPagination", paginationResponse(writtenQnaList));
       model.addAttribute("writtenReplyPagination", paginationResponse(writtenReplyList));
 
-      // 데이터 리스트 모델에 추가
       model.addAttribute("createdExams", createdExamList.getContent());
       model.addAttribute("completedExams", completedExamList.getContent());
       model.addAttribute("writtenQna", writtenQnaList.getContent());
@@ -67,6 +82,31 @@ public class MyPageController {
       return "student/student-info";
 
     } else if (sessionUserRole.isTeacher()) {
+
+      Pageable pendingPageable = PageRequest.of(pagePending, pageable.getPageSize());
+      Pageable createdPageable = PageRequest.of(pageCreated, pageable.getPageSize());
+      Pageable qnaPageable = PageRequest.of(pageQna, pageable.getPageSize());
+      Pageable replyPageable = PageRequest.of(pageReply, pageable.getPageSize());
+
+      Page<TeacherMyPagePendingExamDTO> pendingExamList = teacherMyService.getTeacherMyPendingExamInfo(
+          memberSeq, pendingPageable);
+      Page<TeacherMyPageExamHistoryDTO> examHistoryList = teacherMyService.getTeacherMyPageExamHistoryInfo(
+          memberSeq, createdPageable);
+      Page<TeacherMyPageWrittenQnaDTO> writtenQnaList = teacherMyService.getTeacherMyPageWrittenQnaInfo(
+          memberSeq, qnaPageable);
+      Page<TeacherMyPageWrittenReplyDTO> writtenReplyList = teacherMyService.getTeacherMyPageWrittenReplyInfo(
+          memberSeq, replyPageable);
+
+      model.addAttribute("pendingExamPagination", paginationResponse(pendingExamList));
+      model.addAttribute("examHistoryPagination", paginationResponse(examHistoryList));
+      model.addAttribute("writtenQnaPagination", paginationResponse(writtenQnaList));
+      model.addAttribute("writtenReplyPagination", paginationResponse(writtenReplyList));
+
+      model.addAttribute("pendingExam", pendingExamList.getContent());
+      model.addAttribute("examHistory", examHistoryList.getContent());
+      model.addAttribute("writtenQna", writtenQnaList.getContent());
+      model.addAttribute("writtenReply", writtenReplyList.getContent());
+
       return "teacher/teacher-info";
     }
     return "/index";
