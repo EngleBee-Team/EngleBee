d632cfec58dfc500edbfa5182c04ade5562d579b
wonjun <wonjon0930@daum.net>
2024-09-07 12:19:27 +0900

    fix: 컨벤션에 맞게 어노테이션 위치 수정

--- src/main/java/com/beelinkers/englebee/global/GptAsyncConfig.java
+++ src/main/java/com/beelinkers/englebee/global/GptAsyncConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Configuration
 @EnableAsync
+@Configuration
 public class GptAsyncConfig {
 
   @Bean(name = "gptThreadPoolTask")
