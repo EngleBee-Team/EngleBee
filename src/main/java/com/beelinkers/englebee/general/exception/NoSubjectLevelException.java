1f97f0a0855e206e53f79a987e9fbeb64a1584fa
ram9611 <66862342+ram9611@users.noreply.github.com>
2024-09-07 10:39:55 +0900

    [feat-general-lecture-all] 선생님 메인 페이지에서 수업 등록 구현 (#66)
    * refactor: 과목 레벨 null값 입력시 예외 수정
    
    * chore: lecture.js 추가
    
    * feat: 과목 레벨 예외 처리 추가
    
    * feat: 과목 레벨 예외 추가
    
    * feat: 미확인 멤버 예외, 과목 레벨 예외 처리하는 로직 생성
    
    * feat: 수업 등록 js 로직 생성
    
    * feat: 수업 등록 뷰 적용
    
    * chore: 공통 script 불필요한 js 파일 제거
    
    * chore: admin-chart js 파일 추가

---
+++ src/main/java/com/beelinkers/englebee/general/exception/NoSubjectLevelException.java
@@ -0,0 +1,8 @@
+package com.beelinkers.englebee.general.exception;
+
+public class NoSubjectLevelException extends RuntimeException {
+
+  public NoSubjectLevelException(String message) {
+    super(message);
+  }
+}
