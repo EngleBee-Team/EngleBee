7babdbc6b98d5060233269cc4923fbb04a873f0d
yeong1203 <cozyluv117@gmail.com>
2024-09-09 04:10:47 +0900

    feat: 선생님 메인 페이지 관련 controller 구현

--- src/main/java/com/beelinkers/englebee/general/controller/page/MainPageController.java
+++ src/main/java/com/beelinkers/englebee/general/controller/page/MainPageController.java
@@ -3,7 +3,12 @@ package com.beelinkers.englebee.general.controller.page;
 import com.beelinkers.englebee.auth.annotation.LoginMember;
 import com.beelinkers.englebee.auth.domain.entity.Role;
 import com.beelinkers.englebee.auth.oauth2.session.SessionMember;
+import com.beelinkers.englebee.general.domain.entity.LectureStatus;
+import com.beelinkers.englebee.general.domain.repository.LectureRepository;
 import com.beelinkers.englebee.general.service.MainPageService;
+import com.beelinkers.englebee.student.dto.response.StudentMainPageLectureDTO;
+import com.beelinkers.englebee.student.service.StudentMainService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -16,6 +21,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainPageController {
 
   private final MainPageService mainPageService;
+  private final StudentMainService studentMainService;
+  private final LectureRepository lectureRepository;
 
   @GetMapping("/main")
   public String getMainPage(@LoginMember SessionMember sessionMember, Model model) {
@@ -24,12 +31,26 @@ public class MainPageController {
     }
 
     Long memberSeq = sessionMember.getSeq();
+    List<StudentMainPageLectureDTO> lectures = studentMainService.getOngoingLectureInfo(
+        memberSeq, null, LectureStatus.CREATED);
+
+    // 강의가 존재하는 경우 첫 번째 강의 정보를 모델에 전달
+    if (!lectures.isEmpty()) {
+      Long lectureSeq = lectures.get(0).getLectureSeq(); // 첫 번째 강의의 lectureSeq 가져오기
+      model.addAttribute("lectureSeq", lectureSeq); // lectureSeq를 모델에 추가
+//      log.info("lectureSeq : {}", lectureSeq);
+    } else {
+      model.addAttribute("lectureSeq", null); // 강의가 없을 경우 null 전달
+    }
+
     model.addAttribute("nickname", mainPageService.getNickname(memberSeq));
     model.addAttribute("memberSeq", memberSeq);
+    model.addAttribute("lectures", lectures);
 
     Role sessionUserRole = sessionMember.getRole();
     if (sessionUserRole.isStudent()) {
       return "student/student-main";
+
     } else if (sessionUserRole.isTeacher()) {
       return "teacher/teacher-main";
     }
